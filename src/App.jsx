d3f463d9aeff182c20136e70400bde86d53138ac
SSO <sso@d.co>
2025-04-05 13:46:55 +0200

    splited App.jsx file into multiple sub-components, added example row of non JSX in index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,17 @@
-import { useState } from 'react';
-import CoreConcept from './components/CoreConcept';
+import CoreConsepts from './components/CoreConcepts';
+import Examples from './components/Examples';
 import Header from './components/Header/Header';
-import TabButton from './components/TabButton';
-import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
-	const [selectedTopic, setSelectedTopic] = useState();
-
-	function handleSelect(selectedButton) {
-		// console.log(selectedButton + ' was pressed!');
-		setSelectedTopic(selectedButton);
-	}
 	return (
-		<div>
+		// Using fragment instead of div
+		<>
 			<Header />
 			<main>
-				<section id='core-concepts'>
-					<h2>Time to get started!</h2>
-					<ul>
-						{CORE_CONCEPTS.map((conceptItem) => (
-							<CoreConcept key={conceptItem.title} {...conceptItem} />
-						))}
-					</ul>
-				</section>
-				<section id='examples'>
-					<h2>Examples</h2>
-					<menu>
-						<TabButton
-							isSelected={selectedTopic === 'components'}
-							onSelect={() => handleSelect('components')}
-						>
-							Components
-						</TabButton>
-						<TabButton
-							isSelected={selectedTopic === 'jsx'}
-							onSelect={() => handleSelect('jsx')}
-						>
-							JSX
-						</TabButton>
-						<TabButton
-							isSelected={selectedTopic === 'props'}
-							onSelect={() => handleSelect('props')}
-						>
-							Props
-						</TabButton>
-						<TabButton
-							isSelected={selectedTopic === 'state'}
-							onSelect={() => handleSelect('state')}
-						>
-							State
-						</TabButton>
-					</menu>
-					{!selectedTopic ? (
-						<p>Plese select a topic.</p>
-					) : (
-						<div id='tab-content'>
-							<h3>{EXAMPLES[selectedTopic].title}</h3>
-							<p>{EXAMPLES[selectedTopic].description}</p>
-							<pre>
-								<code>{EXAMPLES[selectedTopic].code}</code>
-							</pre>
-						</div>
-					)}
-				</section>
+				<CoreConsepts />
+				<Examples />
 			</main>
-		</div>
+		</>
 	);
 }
 
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,19 @@
+import { CORE_CONCEPTS } from '../data';
+import CoreConcept from './CoreConcept';
+
+function CoreConsepts() {
+	return (
+		<>
+			<section id='core-concepts'>
+				<h2>Time to get started!</h2>
+				<ul>
+					{CORE_CONCEPTS.map((conceptItem) => (
+						<CoreConcept key={conceptItem.title} {...conceptItem} />
+					))}
+				</ul>
+			</section>
+		</>
+	);
+}
+
+export default CoreConsepts;
