d3d78e65f2ad4f77c9c3179d753ebcd90a783618
SSO <sso@d.co>
2025-04-05 20:51:05 +0200

    Added new Tabs component to reuse tabs

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { EXAMPLES } from '../data';
 import Section from './Section';
 import TabButton from './TabButton';
+import Tabs from './Tabs';
 
 function Examples() {
 	const [selectedTopic, setSelectedTopic] = useState();
@@ -25,33 +26,38 @@ function Examples() {
 	return (
 		<>
 			<Section title='examples' id='examples'>
-				<menu>
-					<TabButton
-						isSelected={selectedTopic === 'components'}
-						onClick={() => handleSelect('components')}
-					>
-						Components
-					</TabButton>
-					<TabButton
-						isSelected={selectedTopic === 'jsx'}
-						onClick={() => handleSelect('jsx')}
-					>
-						JSX
-					</TabButton>
-					<TabButton
-						isSelected={selectedTopic === 'props'}
-						onClick={() => handleSelect('props')}
-					>
-						Props
-					</TabButton>
-					<TabButton
-						isSelected={selectedTopic === 'state'}
-						onClick={() => handleSelect('state')}
-					>
-						State
-					</TabButton>
-				</menu>
-				{tabContent}
+				<Tabs
+					buttons={
+						<>
+							<TabButton
+								isSelected={selectedTopic === 'components'}
+								onClick={() => handleSelect('components')}
+							>
+								Components
+							</TabButton>
+							<TabButton
+								isSelected={selectedTopic === 'jsx'}
+								onClick={() => handleSelect('jsx')}
+							>
+								JSX
+							</TabButton>
+							<TabButton
+								isSelected={selectedTopic === 'props'}
+								onClick={() => handleSelect('props')}
+							>
+								Props
+							</TabButton>
+							<TabButton
+								isSelected={selectedTopic === 'state'}
+								onClick={() => handleSelect('state')}
+							>
+								State
+							</TabButton>
+						</>
+					}
+				>
+					{tabContent}
+				</Tabs>
 			</Section>
 		</>
 	);
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,10 @@
+function Tabs({ children, buttons }) {
+	return (
+		<>
+			<menu>{buttons}</menu>
+			{children}
+		</>
+	);
+}
+
+export default Tabs;
